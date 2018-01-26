ea0c36bec6fbaded83bdaf0c6227da6914237ee8
Cheng Chang <cheng.chang@team.telstra.com>
2018-01-27 09:54:43 +1100

    add routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import logo from '../assets/logo.svg';
 import './App.css';
 import { TodoContainer } from './todo/todo-container';
+import { AboutContainer } from './about/about-container';
 
 class App extends Component {
   render() {
     return (
-      <TodoContainer />
+      <Router>
+        <div>
+          <span><Link to="/">Todo</Link></span> | 
+          <span><Link to="/about">About</Link></span>
+    
+          <hr/>
+    
+          <Route exact path="/" component={TodoContainer}/>
+          <Route path="/about" component={AboutContainer}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/app/about/about-container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function AboutContainer(props) {
+    return (
+        <div>
+            <h2>Copy rights</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe explicabo atque ullam vel incidunt, eaque eum qui obcaecati soluta sint sunt tempora ab laborum animi alias error perferendis. Nihil accusantium ex ipsam excepturi reprehenderit officiis id veniam laboriosam beatae saepe? Asperiores dignissimos laborum ut nemo suscipit! Vitae exercitationem consequatur alias.</p>
+        </div>
+    );    
+}
