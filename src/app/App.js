127b85562569ed0b1c43e802674840145940043b
Cheng Chang <cheng.chang@team.telstra.com>
2018-02-16 21:23:10 +1100

    add react-router-redux

--- src/app/App.js
+++ src/app/App.js
@@ -2,22 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoContainer from './todo/todo-container';
 
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { combineReducers } from 'redux'
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import { Route, Link } from "react-router-dom";
 
 import { todos } from './todo/reducers';
 
-const todoApp = combineReducers({
-  todos
-})
-const store = createStore(todoApp)
+const history = createHistory()
+const middleware = routerMiddleware(history)
 
+const todoApp = combineReducers({
+  todos,
+  router: routerReducer
+})
+const store = createStore(todoApp, applyMiddleware(middleware))
+
+const About = () => (
+  <h1>about</h1>
+)
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <TodoContainer />
+        { /* ConnectedRouter will use the store from Provider automatically */}
+        <ConnectedRouter history={history}>
+          <div>
+            <ul>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/todo">TodoList</Link>
+              </li>
+            </ul>
+            <hr />
+
+            <Route exact path="/" component={TodoContainer} />
+            <Route path="/about" component={About} />
+            <Route path="/todo" component={TodoContainer} />
+          </div>
+        </ConnectedRouter>
       </Provider>
     );
   }
